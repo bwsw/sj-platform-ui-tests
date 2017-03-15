9897d16992c79f0e261ce0123194881fc7f2cbe4
MariaViktorenko <viktorenko_me@bw-sw.com>
2017-03-15 13:04:03 +0700

    Provider connection test added

---
+++ src/test/java/com/bwsw/sj/uitests/tests/ProviderConnectionTests.java
@@ -0,0 +1,28 @@
+package com.bwsw.sj.uitests.tests;
+
+
+import com.bwsw.sj.uitests.model.ProviderData;
+import org.openqa.selenium.By;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class ProviderConnectionTests extends TestBase {
+
+    ProviderData providerData = new ProviderData("zookeeper-auto-connection" + System.currentTimeMillis(), "Zookeeper-auto description"  + System.currentTimeMillis(), "176.120.25.19:2181");
+
+
+    @BeforeMethod
+    public void ensurePreconditions() {
+
+        applicationManager.getProviderHelper().createProvider(providerData);
+    }
+
+    @Test
+    public void testProviderConnection() {
+        applicationManager.getBaseHelper().refreshPage();
+        applicationManager.getProviderHelper().goToProvidersPage();
+        applicationManager.getBaseHelper().waitForElement(By.xpath("//html"));
+        applicationManager.getProviderHelper().connectionProvider(providerData);
+        applicationManager.getProviderHelper().checkConnectionMessage(providerData);
+        }
+}
